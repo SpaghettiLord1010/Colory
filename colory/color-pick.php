4a82a8657dea476016e0e7e90164b8eadcffffe9
SpaghettiLord1010 <50876603+SpaghettiLord1010@users.noreply.github.com>
2021-02-26 19:20:27 +0100

    Version 1.0 Release
    Version 1.0 now released :D

---
+++ colory/color-pick.php
@@ -0,0 +1,10 @@
+<html>
+<center>
+<form action='result.php' method='get'>
+<input type="color" name="color"></input>
+<br/>
+<input type="hidden" name="bgcolor" value=<?php echo $_GET['bgcolor']; ?>></input>
+<input type="submit" value="Guess!"></input>
+</form>
+<center>
+</html>
---
+++ colory/result.php
@@ -0,0 +1,29 @@
+<html>
+<center>
+<?php
+$color = $_GET['color'];
+$backcolor = $_GET['bgcolor'];
+
+list($colorr, $colorg, $colorb) = sscanf($color, "#%02x%02x%02x");
+list($backcolorr, $backcolorg, $backcolorb) = sscanf($backcolor, "#%02x%02x%02x");
+
+
+
+echo "<table style='background-color:" . $backcolor . "'><th>";
+echo "The exact color was: " . $backcolor . "<br/>Which is in RGB: " . $backcolorr . ", " . $backcolorg . ", " . $backcolorb . ".";
+echo "</th></table>";
+echo "<br/>";
+echo "<table style='background-color:" . $color . "'><th>";
+echo "The guessed color was: " . $color  . "<br/>Which is in RGB: " . $colorr . ", " . $colorg . ", " . $colorb . ".";
+echo "</th></table>";
+echo "<br/><br/>";
+echo "<h1>That is an accuracy of:</h1><br/><br/><hr/><br/><br/>";
+if($colorr >= $backcolorr){echo $backcolorr/$colorr*100 . "% for RED <br/>";}
+else{echo $colorr/$backcolorr*100 . "% for RED <br/>";}
+if($colorg >= $backcolorg){echo $backcolorg/$colorg*100 . "% for GREEN <br/>";}
+else{echo $colorg/$backcolorg*100 . "% for GREEN <br/>";}
+if($colorb >= $backcolorb){echo $backcolorb/$colorb*100 . "% for BLUE <br/>";}
+else{echo $colorb/$backcolorb*100 . "% for BLUE <br/>";}
+?>
+<center>
+</html>
